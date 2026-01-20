1ea4ca22e76d26da5f583823473b79c4007fe028
Antoine Paris <paris.antoine.paris@gmail.com>
2026-01-20 20:13:50 +0100

    Rewrite read_leaf_interruptv2 to write to csv instead

--- archives/read_leaf_interruptv2.py
+++ archives/read_leaf_interruptv2.py
@@ -1,17 +1,19 @@
-import Encoder
 import time
+
 from datetime import datetime
+
+import Encoder
 
 e1 = Encoder.Encoder(6, 13)
 e2 = Encoder.Encoder(16, 19)
-e3 = Encoder.Encoder(11,  9)
-e4 = Encoder.Encoder(25,  8)
+e3 = Encoder.Encoder(11, 9)
+e4 = Encoder.Encoder(25, 8)
 e5 = Encoder.Encoder(27, 22)
 e6 = Encoder.Encoder(18, 24)
 
 encoder = [e1, e2, e3, e4, e5, e6]
 
-log_file_path = "/home/pi/Desktop/LBIR1251/leaf_log_all.txt"
+log_file_path = "data.txt"
 
 while (True):
     current_time = datetime.now().strftime("%d%B%Y\t%H:%M:%S")
---
+++ record_leaf_growth.py
@@ -0,0 +1,46 @@
+import csv
+import time
+import os.path
+
+from datetime import datetime
+
+import Encoder
+
+pot1 = Encoder.Encoder(6, 13)
+pot2 = Encoder.Encoder(16, 19)
+pot3 = Encoder.Encoder(11, 9)
+pot4 = Encoder.Encoder(25, 8)
+pot5 = Encoder.Encoder(27, 22)
+pot6 = Encoder.Encoder(18, 24)
+
+pots = [pot1, pot2, pot3, pot4, pot5, pot6]
+
+fname = 'data/data.csv'
+
+if not os.path.isfile(fname):
+    print("Output file does not exists.")
+    print("Creating and writing header.")
+    
+    with open(fname, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, delimiter=';')
+        
+        # Write header
+        writer.writerow(['time', 'pot_1', 'pot_2', 'pot_3', 'pot_4', 'pot_5', 'pot_6'])
+else:
+    print("Output file already exists.")
+    print("I will append data.")
+    
+# Open in append mode, allows to re-start the script without destroying
+# the data if needed (e.g., if something goes wrong in the middle of the
+# experiment)
+with open(fname, 'a', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    
+    while True:
+        row = [datetime.now().strftime("%d-%m-%Y %H:%M:%S")] \
+            + [pot.read() for pot in pots]     
+        
+        print("Writing row: " + str(row))
+        
+        writer.writerow(row)
+        time.sleep(60)
